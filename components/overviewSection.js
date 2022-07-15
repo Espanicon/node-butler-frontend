ddb061030da664d47b746aaceb545bf0b7d43eff
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-07-14 23:59:06 -0400

    building

--- components/overviewSection.js
+++ components/overviewSection.js
@@ -22,6 +22,35 @@ const MOCK_DATA = {
   p2pEndpoint: "127.0.0.1:9000"
 };
 
+const DETAILSJSON = {
+  representative: {
+    logo: {
+      logo_256: "http://somesite.com/logo-small.jpg",
+      logo_1024: "http://somesite.com/logo-big.jpg",
+      logo_svg: "http://somesite.com/logo.svg"
+    },
+    media: {
+      steemit: "",
+      twitter: "",
+      youtube: "",
+      facebook: "",
+      github: "",
+      reddit: "",
+      keybase: "",
+      telegram: "",
+      wechat: ""
+    }
+  },
+  server: {
+    location: {
+      country: "USA",
+      city: "Houston"
+    },
+    server_type: "cloud",
+    api_endpoint: "127.0.0.1:9000"
+  }
+};
+
 function Hr() {
   return (
     <div
@@ -67,9 +96,70 @@ export default function OverviewSection({ activeSection }) {
         </div>
       </div>
       <Hr />
-      <h2>Bonded Info:</h2>
-      <Hr />
-      <h2>Details.json:</h2>
+      <div className={styles.defaultSection}>
+        <h2>Bonded Info:</h2>
+        <Hr />
+      </div>
+      <div className={styles.defaultSection}>
+        <h2>Details.json:</h2>
+        <p>
+          The{" "}
+          <a
+            href="https://docs.icon.community/v/icon1/references/reference-manuals/json-standard-for-p-rep-detailed-information"
+            target="_blank"
+          >
+            <b>details.json</b>
+          </a>{" "}
+          is a file that you can defined in your Prep on-chain data, it will be
+          used by third parties to fetch information about your node. Use the
+          following format and put the file in a route accesible on the internet
+          (you can host it on your team website, github, etc) and then update
+          your Prep settings with a link to it.
+        </p>
+        <div className={styles.codeBlockContainer}>
+          <code className={styles.codeBlock}>
+            &#123;
+            <br />
+            &#160;representative: &#123;
+            <br />
+            &#160;&#160;logo:&#123;
+            <br />
+            &#160;&#160;&#160;logo_256: "http://somesite.com/logo-small.jpg",
+            <br />
+            &#160;&#160;&#160;logo_1024: "http://somesite.com/logo-big.jpg",
+            <br />
+            &#160;&#160;&#160;logo_svg: "http://somesite.com/logo.svg"
+            <br />
+            &#160;&#160;&#125;,
+            <br />
+            &#160;&#160;media: &#123;
+            <br />
+            &#160;&#160;&#160;steemit: "",
+            <br />
+            &#160;&#160;&#160;twitter: "",
+            <br />
+            &#160;&#160;&#160;youtube: "",
+            <br />
+            &#160;&#160;&#160;facebook: "",
+            <br />
+            &#160;&#160;&#160;github: "",
+            <br />
+            &#160;&#160;&#160;reddit: "",
+            <br />
+            &#160;&#160;&#160;keybase: "",
+            <br />
+            &#160;&#160;&#160;telegram: "",
+            <br />
+            &#160;&#160;&#160;wechat: ""
+            <br />
+            &#160;&#160;&#125;
+            <br />
+            &#125;
+          </code>
+        </div>
+        <Hr />
+      </div>
+      <div className={styles.defaultSection}>{/* c */}</div>
     </div>
   );
 }
