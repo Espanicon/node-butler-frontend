d1444b1daa1bb38907cc795c74b7ab669c445ac7
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-08-12 22:41:48 -0400

    working on network proposal section

--- components/networkProposalsSection.js
+++ components/networkProposalsSection.js
@@ -113,102 +113,112 @@ export default function NetworkProposalsSection({ localData }) {
       ) : (
         <div className={styles.section}>
           {networkProposals.map((eachProposal, index) => {
-            let statusTitle =
-              DATA.statusTypes[eachProposal.status] == null
-                ? "UNKNOWN"
-                : DATA.statusTypes[eachProposal.status];
-            let proposalType =
-              DATA.proposalTypes[eachProposal.contents.type] == null
-                ? "UNKNOWN"
-                : DATA.proposalTypes[eachProposal.contents.type];
-            let styledStatus =
-              DATA.styledStatus[eachProposal.status] == null
-                ? styles.yellowBorder
-                : DATA.styledStatus[eachProposal.status];
-            let imgSrc =
-              DATA.imgSrc[eachProposal.status] == null
-                ? DATA.imgSrc["0x0"]
-                : DATA.imgSrc[eachProposal.status];
-
             return (
-              <div
-                className={`${styles.cardContainer} ${styledStatus}`}
+              <CustomCard
+                eachProposal={eachProposal}
+                handleModalOnOpen={handleModalOnOpen}
+                index={index}
                 key={uuidv4()}
-                onClick={() => handleModalOnOpen(index)}
-              >
-                <div className={styles.cardTitle}>
-                  <p>{eachProposal.contents.title}</p>
-                </div>
-                <div className={styles.cardStatusContainer}>
-                  <p className={styles.cardStatusInfo}>
-                    <b>Status: </b>
-                    {statusTitle}
-                  </p>
-                  <img
-                    src={imgSrc}
-                    className={styles.cardStatusImage}
-                    alt="proposal status"
-                  />
-                </div>
-              </div>
+              />
             );
           })}
         </div>
       )}
-      <GenericModal isOpen={isOpen} onClose={handleModalOnClose}>
-        {networkProposals == false ||
-        networkProposals == null ||
-        proposalIndex === false ? (
-          <div
-            className={`${styles.proposalInfoContainer} ${styles.proposalInfoContainerIsWaiting}`}
-          >
-            <div className={styles.imgLoading}>
-              {[1, 2, 3, 4, 5].map(foo => (
-                <div className={styles.imgLoadingItem} key={uuidv4()}></div>
-              ))}
-            </div>
+      <CustomModal
+        isOpen={isOpen}
+        onClose={handleModalOnClose}
+        proposals={networkProposals}
+        index={proposalIndex}
+      />
+    </div>
+  );
+}
+
+function CustomCard({ eachProposal, handleModalOnOpen, index }) {
+  let statusTitle =
+    DATA.statusTypes[eachProposal.status] == null
+      ? "UNKNOWN"
+      : DATA.statusTypes[eachProposal.status];
+  let proposalType =
+    DATA.proposalTypes[eachProposal.contents.type] == null
+      ? "UNKNOWN"
+      : DATA.proposalTypes[eachProposal.contents.type];
+  let styledStatus =
+    DATA.styledStatus[eachProposal.status] == null
+      ? styles.yellowBorder
+      : DATA.styledStatus[eachProposal.status];
+  let imgSrc =
+    DATA.imgSrc[eachProposal.status] == null
+      ? DATA.imgSrc["0x0"]
+      : DATA.imgSrc[eachProposal.status];
+
+  return (
+    <div
+      className={`${styles.cardContainer} ${styledStatus}`}
+      onClick={() => handleModalOnOpen(index)}
+    >
+      <div className={styles.cardTitle}>
+        <p>{eachProposal.contents.title}</p>
+      </div>
+      <div className={styles.cardStatusContainer}>
+        <p className={styles.cardStatusInfo}>
+          <b>Status: </b>
+          {statusTitle}
+        </p>
+        <img
+          src={imgSrc}
+          className={styles.cardStatusImage}
+          alt="proposal status"
+        />
+      </div>
+    </div>
+  );
+}
+
+function CustomModal({ isOpen, onClose, proposals, index }) {
+  return (
+    <GenericModal isOpen={isOpen} onClose={onClose}>
+      {proposals == false || proposals == null || index === false ? (
+        <div
+          className={`${styles.proposalInfoContainer} ${styles.proposalInfoContainerIsWaiting}`}
+        >
+          <div className={styles.imgLoading}>
+            {[1, 2, 3, 4, 5].map(foo => (
+              <div className={styles.imgLoadingItem} key={uuidv4()}></div>
+            ))}
           </div>
-        ) : (
-          <div className={styles.proposalContainer}>
-            <div className={styles.proposalContainerHeader}>
-              <h2>{networkProposals[proposalIndex].contents.title}</h2>
-              <ul>
-                <li>
-                  Proposer: {networkProposals[proposalIndex].proposerName}
-                </li>
-                <li>
-                  Start (block height):{" "}
-                  {parseInt(networkProposals[proposalIndex].startBlockHeight)}
-                </li>
-                <li>
-                  End (block height):{" "}
-                  {parseInt(networkProposals[proposalIndex].endBlockHeight)}
-                </li>
-                <li>Proposal id: {networkProposals[proposalIndex].id}</li>
-                <li>
-                  Status:{" "}
-                  {DATA.statusTypes[networkProposals[proposalIndex].status]}
-                </li>
-                <li>
-                  Type:{" "}
-                  {
-                    DATA.proposalTypes[
-                      networkProposals[proposalIndex].contents.type
-                    ]
-                  }
-                </li>
-              </ul>
-            </div>
-            <Hr />
-            <div
-              className={styles.proposalContainerBody}
-              dangerouslySetInnerHTML={{
-                __html: networkProposals[proposalIndex].contents.description
-              }}
-            ></div>
-          </div>
-        )}
-      </GenericModal>
+        </div>
+      ) : (
+        <ModalContent proposals={proposals} index={index} />
+      )}
+    </GenericModal>
+  );
+}
+function ModalContent({ proposals, index }) {
+  return (
+    <div className={styles.proposalContainer}>
+      <div className={styles.proposalContainerHeader}>
+        <h2>{proposals[index].contents.title}</h2>
+        <ul>
+          <li>Proposer: {proposals[index].proposerName}</li>
+          <li>
+            Start (block height): {parseInt(proposals[index].startBlockHeight)}
+          </li>
+          <li>
+            End (block height): {parseInt(proposals[index].endBlockHeight)}
+          </li>
+          <li>Proposal id: {proposals[index].id}</li>
+          <li>Status: {DATA.statusTypes[proposals[index].status]}</li>
+          <li>Type: {DATA.proposalTypes[proposals[index].contents.type]}</li>
+        </ul>
+      </div>
+      <Hr />
+      <div
+        className={styles.proposalContainerBody}
+        dangerouslySetInnerHTML={{
+          __html: proposals[index].contents.description
+        }}
+      ></div>
     </div>
   );
 }
