a1384ef86eb77210c0a14b1ebc11da2ebbf4b9c3
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-08-20 17:28:28 -0400

    created bare structure for contract explorer section

--- components/contractExplorerSection.js
+++ components/contractExplorerSection.js
@@ -2,25 +2,54 @@ import { useState } from "react";
 import styles from "../styles/contractExplorerSection.module.css";
 import { Hr } from "./customComponents";
 
+const MOCK_DATA = {
+  textarea: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.`
+};
 export default function ContractExplorerSection({ localData }) {
+  const [scoreInput, setScoreInput] = useState("");
+  const [scoreData, setScoreData] = useState(MOCK_DATA.textarea);
+
+  function handleScoreInputChange(evnt) {
+    const value = evnt.target.value;
+    setScoreInput(value);
+  }
   return (
     <div className={styles.main}>
       <h2>Contract Explorer</h2>
-      <p>
-        Contract Explorer paragraph Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-      </p>
+      <p>Add an ICON smart contract address (SCORE) in the following input.</p>
+      <ul>
+        <li>
+          SCORE Address:{" "}
+          <input
+            type="text"
+            name="scoreInput"
+            value={scoreInput}
+            onChange={handleScoreInputChange}
+          />
+        </li>
+      </ul>
+
+      <p>SCORE details:</p>
+      <textarea className={styles.textarea}>{scoreData}</textarea>
     </div>
   );
 }
