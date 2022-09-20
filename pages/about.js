f56a098e69ec449439ca762a1baf9e5d6fe88f1f
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-09-20 18:23:00 -0400

    refactored pages

--- pages/about.js
+++ pages/about.js
@@ -14,18 +14,45 @@ export default function About() {
       <div className={styles.container}>
         <Section>
           <SectionWithLogo path="/images/espanicon-logo.png">
-            <h1>FAQ</h1>
+            <h1>About Node Butler</h1>
             <p>
-              <b>Node Butler</b> is a web app intented for node
-              validators..."Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est
-              laborum."
+              <b>Node Butler</b> is a web app intented to facilitate the
+              management of validator nodes in the ICON Network. It allows node
+              managers to easily execute common task like editing on chain node
+              data (node name, email, country, etc), managing wallets for
+              bonding, voting on network proposals, signing transactions on any
+              SCORE (smart contract) in the ICON Network, etc.
             </p>
+            <p>The app is divided in the following sections:</p>
+            <ul>
+              <li>
+                <b>Overview Section:</b> This section shows general on-chain
+                data related to the node and allows for this data to be modified
+                by signing a transaction for the <b>SetPRep</b> method with the
+                node wallet using either ICONex or Hana. In this section you
+                will also be able to add/delete wallets that are able to bond
+                for your node by signing a transaction on the{" "}
+                <b>SetBonderlist</b> method.
+              </li>
+              <li>
+                <b>CPS Proposals:</b> This section shows a summary of all the
+                proposals that have been submitted to ICON decentralized funding
+                program (<b>Contribution Proposal System</b>). It displays the
+                state of the proposal as well as the votes of each Prep.
+              </li>
+              <li>
+                <b>Network Proposals:</b> This sections shows a summary of all
+                the network proposals in the ICON Network and allows the Preps
+                to vote to approve or reject network proposals that are
+                currently active.
+              </li>
+              <li>
+                <b>Contract Explorer:</b> This section allows anyone to sign
+                transactions on any SCORE (Smart Contract) in the ICON Network
+                with the addresses that are currently available in your Hana or
+                ICONex wallet.
+              </li>
+            </ul>
           </SectionWithLogo>
         </Section>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -12,48 +12,39 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <Section>
-        <SectionWithLogo path="/images/espanicon-logo.png">
-          <div className={styles.container}>
-            <h2 className={styles.title}>P-rep for the ICON Network</h2>
-            <p>
-              <b>Espanicon</b> is a validator node in the <b>ICON Network</b>.
-              Our goal is to participate in the chain governance of the network
-              and expand the <b>ICON</b> ecosystem by building projects in it
-              and collaborate with other validators.
-            </p>
-            <p>
-              If you want to support our work you can do it by simply allocating
-              votes to the <b>Espanicon</b> node with your staked <b>ICX</b>,
-              every vote counts!, we highly appreciate your support!.
-            </p>
-          </div>
-        </SectionWithLogo>
-      </Section>
-      <Section colored>
-        <SectionWithLogo float="left" path="/images/icon-logo.png">
+        <SectionWithLogo path="/images/icon-logo.png">
           <div className={styles.container}>
             <h2 className={styles.title}>
-              ICON: Connecting blockchains and communities.
+              Node-Butler makes managing ICON nodes easier!
             </h2>
             <p>
-              <b>ICON</b> is a public blockchain protocol building a
-              hyperconnected future with BTP, a chain-agnostic interoperability
-              solution.
+              <b>Node-Butler</b> is a web-based blockchain application with the
+              goal of making it easier to manage validator nodes in{" "}
+              <b>ICON Network</b>.
             </p>
             <p>
-              ICX is the currency that powers the ICON ecosystem. ICX has a
-              variety of use cases including staking, network governance,
-              collateralization on DeFi platforms, and more.
-            </p>
-            <p>
-              You can learn more about <b>ICON</b> by visiting the following{" "}
-              <a href="https://icon.community/learn/" target="_blank">
+              This project is able to exists thanks to a fund granted by the{" "}
+              <b>Contribution Proposal System</b> a decentralized funding
+              program that exists as a core component of the ICON Network, if
+              you want to create cool projects in ICON and are looking for
+              funding we recommend you give a look to the CPS in the following{" "}
+              <a href="https://cps.icon.community/" target="_blank">
                 link!
               </a>
-              .
+            </p>
+            <p>
+              If you like <b>Node-Butler</b> and want to support our work you
+              can do it by simply allocating votes to the <b>Espanicon</b> node
+              with your staked <b>ICX</b>, every vote counts!, we highly
+              appreciate your support!.
             </p>
           </div>
         </SectionWithLogo>
+        <Link href="/app">
+          <div className={styles.appLink}>
+            <b>Go to Node-Butler app</b>
+          </div>
+        </Link>{" "}
       </Section>
     </Layout>
   );
